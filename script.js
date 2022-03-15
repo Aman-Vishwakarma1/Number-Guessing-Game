486711fd323a5855b92ea4131c1beaa673415fe9
Aman-Vishwakarma1 <95521025+Aman-Vishwakarma1@users.noreply.github.com>
2022-03-15 12:19:00 +0530

    more effecient code

--- script.js
+++ script.js
@@ -1,71 +1,58 @@
 'use strict';
 
-let logicNumber = Math.trunc(Math.random()*20+1)
+let logicNumber = Math.trunc(Math.random() * 20 + 1)
 let score = 20;
 let highscore = 0;
 
+const displayMessage = function(message){
+    document.querySelector('.message').textContent = message;
+}
 
 document.querySelector('.check').addEventListener
-('click', function() {
+    ('click', function () {
 
-    const guessValue = Number(document.querySelector('.guess').value);
+        const guessValue = Number(document.querySelector('.guess').value);
+        // IF PLAYER DON'T GIVE CORRECT NUMBER OR LEFT FIELD BLANK
+        if (!guessValue) {
+            displayMessage('⛔ No Number');
+        }
+        // IF GUESS IS CORRECT AND PLAYER WINS
+        else if (guessValue === logicNumber) {
+            if (score > highscore) {
+                highscore = score;
+            }
+            displayMessage('🎊 Correct Guess You Wins');
+            document.querySelector('.number').textContent = logicNumber;
+            document.querySelector('.highscore').textContent = score;
+            document.querySelector('body').style.backgroundColor = '#60b347';
+            document.querySelector('.number').style.width = '30rem';
 
-    if (!guessValue) {
-        document.querySelector('.message').textContent = '⛔ No Number';
-    }
-    else if (guessValue===logicNumber){
-        if(score>highscore){
-            highscore = score;
         }
-        document.querySelector('.message').textContent = '🎊 Correct Guess You Wins'
-        document.querySelector('.number').textContent = logicNumber;
-        document.querySelector('.highscore').textContent = score;
-        document.querySelector('body').style.backgroundColor = '#60b347';
-        document.querySelector('.number').style.width = '30rem';
-        
-    }
-    else if(guessValue > logicNumber){
-        if(score>1){
-        document.querySelector('.message').textContent = '📈 Too High'
-        score--;
-        document.querySelector('.score').textContent = score;
-        }
-        else {
-            document.querySelector('.message').textContent = 'You Lost the Game'
-            document.querySelector('.score').textContent = 0;
-            document.querySelector('body').style.backgroundColor = 'f70d1a';
-            document.querySelector('.logicNumber').style.width = '30rem';
-        }
-    }
-    else if(guessValue < logicNumber){
-        if(score>1){
-            document.querySelector('.message').textContent = '📉 Too Low'
-            score--;
-            document.querySelector('.score').textContent = score;
+        // IF GUESS IS WRONG
+        else if (guessValue !== logicNumber) {
+            if (score > 1) {
+                displayMessage(guessValue > logicNumber ? "📈 Too High" : "📉 Too Low"), 
+                score--;
+                document.querySelector('.score').textContent = score;
             }
             else {
-                document.querySelector('.message').textContent = 'You Lost the Game'
+                displayMessage('You Lost the Game');
                 document.querySelector('.score').textContent = 0;
-                document.querySelector('body').style.backgroundColor = '';
+                document.querySelector('body').style.backgroundColor = 'f70d1a';
                 document.querySelector('.logicNumber').style.width = '30rem';
             }
-    }
+        }
+    });
+        // PLAY AGAIN BUTTON FUCTIONS AND LOGIC
+        document.querySelector('.again').addEventListener('click', function () {
+            document.querySelector('.number').textContent = '?'
+            score = 20;
+            logicNumber = Math.trunc(Math.random() * 20 + 1);
+            displayMessage('Start guessing...');
+            document.querySelector('.score').textContent = score;
 
+            document.querySelector('.guess').value = '';
 
-});
-
-
-document.querySelector('.again').addEventListener('click', function () {
-
-    document.querySelector('.number').textContent = '?'
-    score = 20;
-    logicNumber = Math.trunc(Math.random() * 20 + 1);
-    document.querySelector('.message').textContent = 'Start guessing...'
-    document.querySelector('.score').textContent = score;
-
-    document.querySelector('.guess').value = '';
-  
-    document.querySelector('body').style.backgroundColor = '#222';
-    document.querySelector('.number').style.width = '15rem';
-    
-});
+            document.querySelector('body').style.backgroundColor = '#222';
+            document.querySelector('.number').style.width = '15rem';
+        });
